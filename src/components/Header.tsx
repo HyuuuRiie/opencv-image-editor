6d3497538cc1373411a7a19ec8c2f19b20cbe35d
Fajri <wahyuseptiadifajri11@gmail.com>
2025-04-10 17:35:51 +0700

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <>
+      <header className="p-3 text-bg-primary">
+        <div className="container-fluid">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <h2 className="text-center text-lg-start text-white">
+              🖼️ Image Editor
+            </h2>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
